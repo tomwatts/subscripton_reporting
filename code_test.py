daecc810811cb00b549e06b69dddb661f2e9aafd
U-AGFAHEALTHCARE\axjsl <axjsl@WTLWA207.agfahealthcare.com>
2015-01-19 17:12:31 -0500

    Adding proper populating of the daily_domain_counts table and don't fail on creating tables when they already exist; just drop them and repopulate.

--- code_test.py
+++ code_test.py
@@ -1,34 +1,39 @@
 #!/usr/bin/python
 
+import math
 import sqlite3
 import time
 
-run_time = time.strftime('%Y-%m-%d %H:%M:%S')
-print("Invoked at " + run_time)
+day = math.floor(time.time() / 60 / 60 / 24)
+print(str(day) + " days since epoch.")
 
 connection = sqlite3.connect('code_test.db')
 cursor = connection.cursor()
 
-domains = dict()
+domain_counts = dict()
 
-for row in cursor.execute('SELECT addr FROM mailing LIMIT 10'):
+#for row in cursor.execute('SELECT addr FROM mailing LIMIT 50'):
+for row in cursor.execute('SELECT addr FROM mailing'):
 	# Chop off username and '@'
 	domain = row[0].split('@')[1]
 
 	# Count occurences of each domain
-	if (domain in domains):
-		domains[domain] += 1
+	if (domain in domain_counts):
+		domain_counts[domain] += 1
 	else:
-		domains[domain] = 1
+		domain_counts[domain] = 1
 	
-print domains
-
+sum = 0
 # Insert each domain into daily_domain_counts
-for domain in domains:
-	print domain + count
+for domain, count in domain_counts.items():
+	#print domain + " " + str(count)
+	cursor.execute('INSERT INTO daily_domain_counts (domain, day, count) VALUES ( ?, ?, ?)', \
+		(domain, day, count))
+	sum += count
 
-	#cursor.execute('INSERT INTO daily_domain_counts (domain, date, count) VALUES ( ?, ?, ?)', \
-		#domain.key, date, domain.value)
+connection.commit()
+
+print "Finished adding daily domain counts."
 
 # TODO: Query for top 50 domains, summing count for each day
 # TODO: Sort and calculate increase vs. count 30 days ago
--- db_create.py
+++ db_create.py
@@ -4,16 +4,20 @@ import sqlite3
 import random
 
 def create_db(connection):
+	print('Creating database...')
 	cursor = connection.cursor()
-	cursor.execute('DROP TABLE mailing')
+	cursor.execute('DROP TABLE IF EXISTS mailing')
 	cursor.execute('CREATE TABLE mailing ( addr VARCHAR(255) NOT NULL )')
+	cursor.execute('DROP TABLE IF EXISTS daily_domain_counts')
 	cursor.execute('CREATE TABLE daily_domain_counts ( '
 		' domain VARCHAR(255) NOT NULL, '
-		' date DATETIME NOT NULL, '
+		' day UNSIGNED INT NOT NULL, '
 		' count UNSIGNED BIG INT NOT NULL DEFAULT 0 )')
 	connection.commit()
+	print ('Done!')
 	
 def populate_db(connection):
+	print('Populating database...')
 	cursor = connection.cursor()
 	domains = []
 
@@ -24,13 +28,14 @@ def populate_db(connection):
 	for x in range(0, 10000000):
 		# Choose a random domain and for email address
 		address = str(x) + '@' + random.choice(domains)
-		if (x % 1000000 == 0):
-			print address
+		#if (x % 1000000 == 0):
+			#print address
 		
 		# Insert into DB
 		cursor.execute('INSERT INTO mailing VALUES (?)', (address,))
 	
 	connection.commit()
+	print ('Done!')
 
 
 connection = sqlite3.connect('code_test.db')
