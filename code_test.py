211a0d507355b09bd65aae014f0fa133639bfdb8
Tom Watts <github@tomwatts.ca>
2015-01-29 22:14:09 -0500

    More code cleanup and fixing a crash when there is no data for 30 days prior.

--- code_test.py
+++ code_test.py
@@ -26,15 +26,24 @@ for address in cursor.execute('SELECT addr FROM mailing'):
 # Insert today's count for each domain into daily_domain_counts
 for domain, count in domain_counts.items():
 	# Overwrite if the count is already there for today's run
-	cursor.execute('INSERT OR REPLACE INTO daily_domain_counts (domain, day, count) VALUES (:domain, :day, :count)', \
-		{"domain" : domain, "day" : day, "count" : count})
+	cursor.execute("""
+		INSERT OR REPLACE INTO daily_domain_counts (
+			domain, day, count
+		) VALUES (
+			:domain, :day, :count
+		)""", {"domain" : domain, "day" : day, "count" : count})
 
 connection.commit()
 
 print "Domain\t\t| Count\t| % Increase"
 
 # Get the top 50 domains by today's count
-cursor.execute('SELECT domain, count FROM daily_domain_counts WHERE day = :day ORDER BY count DESC LIMIT 50', {"day" : day})
+cursor.execute("""
+	SELECT domain, count
+	FROM daily_domain_counts
+	WHERE day = :day
+	ORDER BY count DESC
+	LIMIT 50""", {"day" : day})
 top_fifty_domains = cursor.fetchall()
 
 # Domain ranking counter
@@ -45,11 +54,21 @@ for domain_count in top_fifty_domains:
 	domain = domain_count[0]
 	count = domain_count[1]
 	thirty_days_ago = day - 30
-	cursor.execute('SELECT count FROM daily_domain_counts WHERE day=:thirty_days_ago AND domain = :domain LIMIT 1', {"thirty_days_ago" : thirty_days_ago, "domain" : domain})
-	count_thirty_days_ago = cursor.fetchone()[0]
-	percent_increase = 100 * (count - count_thirty_days_ago) / count_thirty_days_ago
+
+	cursor.execute("""
+		SELECT count FROM daily_domain_counts
+		WHERE day=:thirty_days_ago AND domain = :domain LIMIT 1""", \
+		{"thirty_days_ago" : thirty_days_ago, "domain" : domain})
+	count_thirty_days_ago = cursor.fetchone()
+
+	percent_increase = "N/A"
+
+	if (count_thirty_days_ago is not None):
+		percent_increase = str(100 * \
+			(count - count_thirty_days_ago[0]) / count_thirty_days_ago[0]) + "%"
+
 	# TODO: Sort by percent_increase
-	print str(index) + ". " + domain + "\t| " + str(count) + "\t| " + str(percent_increase) + "%"
+	print str(index) + ". " + domain + "\t| " + str(count) + "\t| " + percent_increase
 
 connection.close()
 
