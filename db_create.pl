39127205383c559ff620d53f0ba9e4d293544461
Tom Watts <github@tomwatts.ca>
2015-03-13 00:21:14 -0400

    Starting conversion to Perl.

--- db_create.pl
+++ db_create.pl
@@ -1,46 +1,57 @@
 #!/usr/bin/perl
 
-import sqlite3
-import random
+use DBI;
+#import random
 
-def create_db(connection):
-	print('Creating database...')
-	cursor = connection.cursor()
-	cursor.execute('''CREATE TABLE IF NOT EXISTS mailing ( addr VARCHAR(255)
-		NOT NULL )''')
-	cursor.execute('''CREATE TABLE IF NOT EXISTS daily_domain_counts (
-		domain VARCHAR(255) NOT NULL,
-		day UNSIGNED INT NOT NULL,
-		count UNSIGNED BIG INT NOT NULL DEFAULT 0,
-		CONSTRAINT daily_domain_counts PRIMARY KEY ( domain, day ))''')
-	connection.commit()
-	print ('Done!')
+sub create_db {
+	print("Creating database...");
+	my $connection = shift;
+	my $statement = $connection->prepare('CREATE TABLE IF NOT EXISTS mailing (
+		addr VARCHAR(255) NOT NULL )');
+	$statement->execute();
+	$statement->finish();
+
+	$statement = $connection->prepare(
+		'CREATE TABLE IF NOT EXISTS daily_domain_counts (
+			domain VARCHAR(255) NOT NULL,
+			day UNSIGNED INT NOT NULL,
+			count UNSIGNED BIG INT NOT NULL DEFAULT 0,
+			CONSTRAINT daily_domain_counts PRIMARY KEY ( domain, day ))');
+	$statement->execute();
+	$statement->finish();
+
+	print ("done!\n");
+}
 	
-def populate_db(connection):
-	print('Populating database...')
-	cursor = connection.cursor()
-	domains = []
+sub populate_db {
+	print("Populating database...");
+	my $connection = shift;
+	my @domains = ();
 
-	for x in range(0, 100000):
+	foreach (0..100000) {
 		# Generate a domain and add it to the list of domains
-		domains.append(str(x) + ".com")
+#		$domains.append(str(x) + ".com")
+	}
 
-	for x in range(0, 10000000):
+	foreach (0..10000000) {
 		# Choose a random domain and for email address
-		address = str(x) + '@' + random.choice(domains)
+#		address = str(x) + '@' + random.choice(domains)
 		#if (x % 1000000 == 0):
 			#print address
 		
 		# Insert into DB
-		cursor.execute('INSERT INTO mailing (addr) VALUES (?)', (address,))
+#		my $statement = $connection->prepare(
+#			'INSERT INTO mailing (addr) VALUES (?)', (address,));
+	}
 	
-	connection.commit()
-	print ('Done!')
+#	connection.commit()
+	print ("done!\n");
+}
 
-connection = sqlite3.connect('subscriptions.db')
+my $connection = DBI->connect('dbi:SQLite:subscriptions.db');
 
-create_db(connection)
-populate_db(connection)
+create_db($connection);
+populate_db($connection);
 
-connection.close()
+$connection->disconnect();
 
